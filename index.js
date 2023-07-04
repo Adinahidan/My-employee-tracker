eb4bd0376b31e993e19745c3f2d92e4de0effc57
Adina <yna_dulci@yahoo.com>
2023-07-04 23:17:16 +0100

    updated startApp function

--- index.js
+++ index.js
@@ -13,20 +13,25 @@ function startApp() {
             message: "What would you like to do?",
             name: "choice",
             type: "list",
-            choices: ["View Departments", "View Roles", "Add Role", "Quit"]
+            choices: ["View All Departments", "View All Roles", "View All Employees", "Add Department", "Add Employee", "Add role", "Quit"]
         }
     ]).then(answer => {
-        if(answer.choice == "View Departments") {
-            viewDept()
-        }else if (answer.choice == "View Roles") {
-            viewRoles()
-        }else if (answer.choice == "Add Role") {
-            addRole()
-        }
-    })
+        if(answer.choice == "View All Departments") {
+            viewAllDept()
+        }else if (answer.choice == "View All Roles") {
+            viewAllRoles()
+        }else if (answer.choice == "View All Employees") {
+            viewAllEmployees()
+        }else if (answer.choice == "Add Department") {
+            addDepartment()
+        }else if (answer.choice == "Add Employee") {
+            addEmployee()
+        }else if (answer.choice =="Add Role"){
+    }       addRole()
+})
 }
 
-function viewDept() {
+function viewAllDept() {
     const sqlString = `
     SELECT *
     FROM department`
@@ -41,7 +46,7 @@ function viewDept() {
     })
 }
 
-function viewRoles() {
+function viewAllRoles() {
     const sqlString = `
     SELECT title, salary, department.name
     FROM role
@@ -56,6 +61,24 @@ function viewRoles() {
         startApp()
     })
 }
+
+function viewAllEmployees() {
+    const sqlString = 
+    `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+     FROM employee
+     LEFT JOIN role ON employee.role_id = role.id
+     LEFT JOIN department ON role.department_id = department.id
+     LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+  
+    db.query(sqlString, (err, data) => {
+      if (err) throw err;
+      console.table(data);
+      console.log("\n");
+  
+      startApp();
+    })
+  }
+  
 
 function loadDepartments() {
     return db.promise().query('SELECT * FROM department');
